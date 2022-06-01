fdd976381551d2e2e98f66e59fe579cdeb7097c4
YoungiJ <104750081+YoungiJ@users.noreply.github.com>
2022-06-01 19:37:15 +0900

    Add files via upload

---
+++ 웹프팀플/js/전시회.js
@@ -0,0 +1,21 @@
+function look(obj){
+    obj.src="홈페이지5.jpg"
+}
+function look2(obj){
+    obj.src="홈페이지15.jpg"
+}
+
+
+function look3(obj){
+    obj.src="홈페이지9.jpg"
+}
+function look4(obj){
+    obj.src="홈페이지16.jpg"
+}
+
+function look5(obj){
+    obj.src="홈페이지10.jpg"
+}
+function look6(obj){
+    obj.src="홈페이지17.jpg"
+}
